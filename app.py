f0f36c5c363bcc0ef9707f4ec17db24ed8a2bf3f
sialaichai <sialaichai@gmail.com>
2025-12-15 19:21:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -331,8 +331,8 @@ def main():
 
     # --- 2. Render Login/Handle Status ---
     # The name of the user, the authentication status (True/False/None), and the username
-    name, authentication_status, username = authenticator.login('Login', 'main')
-
+    #name, authentication_status, username = authenticator.login('Login', 'main')
+    name, authentication_status, username = authenticator.login('Login')
     # Status check and core logic starts here
     if st.session_state["authentication_status"]:
         # User is logged in
